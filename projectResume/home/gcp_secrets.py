fb070613bb3207eeef199b996ddd65ad28288761
Rahul Singh <rahulsingh594@deloitte.com>
2024-09-13 23:25:33 +0530

    preview button added

--- projectResume/home/gcp_secrets.py
+++ projectResume/home/gcp_secrets.py
@@ -34,34 +34,34 @@
 # or implementing the sample code, visit the AWS docs:
 # https://aws.amazon.com/developer/language/python/
 
-import boto3
-from botocore.exceptions import ClientError
-import json
+# import boto3
+# from botocore.exceptions import ClientError
+# import json
 
 
-def get_secret():
+# def get_secret():
 
-    secret_name = "my_secret_api"
-    region_name = "us-east-1"
+#     secret_name = "my_secret_api"
+#     region_name = "us-east-1"
 
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
+#     # Create a Secrets Manager client
+#     session = boto3.session.Session()
+#     client = session.client(
+#         service_name='secretsmanager',
+#         region_name=region_name
+#     )
 
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        # For a list of exceptions thrown, see
-        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        raise e
+#     try:
+#         get_secret_value_response = client.get_secret_value(
+#             SecretId=secret_name
+#         )
+#     except ClientError as e:
+#         # For a list of exceptions thrown, see
+#         # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+#         raise e
 
-    secret = get_secret_value_response['SecretString']
-    return json.loads(secret)
+#     secret = get_secret_value_response['SecretString']
+#     return json.loads(secret)
 
 
-    # Your code goes here.
+#     # Your code goes here.
